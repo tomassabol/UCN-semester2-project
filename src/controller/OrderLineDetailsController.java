cc3bd6311a9664310ab7702b219d1f46ab185816
ttommmy <93518302+ttommmy@users.noreply.github.com>
2022-04-22 12:09:35 +0200

    Started the gui for create order

---
+++ src/controller/OrderLineDetailsController.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import database.OrderLineDetailsDB;
+import database.interfaces.OrderLineDetailsDBIF;
+import exceptions.NotFoundException;
+import model.Item;
+import model.OrderLine;
+
+/**
+ * @author ttomy
+ * @version 22 Apr 2022
+ */
+public class OrderLineDetailsController {
+
+	/**
+	 * Fields for class OrderLineDetailsController
+	 */
+	private OrderLineDetailsDBIF orderLineDetailsDBIF;
+	
+	/**
+	 * Constructor for class OrderLineDetailsController
+	 * @throws SQLException
+	 */
+	public OrderLineDetailsController() throws SQLException {
+		orderLineDetailsDBIF = new OrderLineDetailsDB();
+	}
+	
+	/**
+	 * Lists all the items for a specific orderLine
+	 * @param id The id of the orderLine
+	 * @return A list of the items in an orderLine
+	 * @throws SQLException
+	 * @throws NotFoundException
+	 */
+	public List<Item> findByOrderLine(int id) throws SQLException, NotFoundException {
+		List<Item> items = orderLineDetailsDBIF.findByOrderLine(id);
+		return items;
+	}
+	
+	/**
+	 * It inserts the details of an orderLine into the database / orderLine id and item id
+	 * @param orderLine The orderLine thats details are getting inserted into the database
+	 * @param item The item that is associated with the orderLine
+	 * @throws SQLException
+	 */
+	public void createOrderLineDetails(OrderLine orderLine, Item item) throws SQLException {
+		orderLineDetailsDBIF.createOrderLineDetails(orderLine, item);
+	}
+}
---
+++ src/view/tableModel/CreateOrderTableModel.java
@@ -0,0 +1,111 @@
+/**
+ * 
+ */
+package view.tableModel;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import controller.OrderController;
+import controller.OrderLineController;
+import model.OrderLine;
+import models.ShoppingItemLine;
+
+/**
+ * @author ttomy
+ * @version 22 Apr 2022
+ */
+public class CreateOrderTableModel extends AbstractTableModel{
+
+	private static final String[] columnNames = {"ID", "Product", "Quantity"}; //TODO: Add price per orderLine
+	
+	private List<OrderLine> orderLines;
+	private OrderController orderCtrl;
+	private OrderLineController orderLineCtrl;
+	private OrderDetailsCtrl orderDetailsCtrl;
+	
+	public CreateOrderTableModel(int id) throws SQLException {
+		orderCtrl = new OrderController();
+		orderLineCtrl = new OrderLineController();
+		orderLines = orderDetailsCtrl.findByOrderId(id);
+	}
+	
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return orderLines.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return columnNames.length;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return columnNames[column];
+	}
+	
+	
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Object value = null;
+		OrderLine orderLine = orderLines.get(rowIndex);
+		switch(columnIndex) {
+			case 0: value = orderLine.getId();
+			case 1: value = orderLine.getProduct();
+			case 2: value = orderLine.getQuantity();
+			default: value = null;
+		}
+		return value;
+	}
+	
+	/**
+	 *  Make cells uneditable
+	 */
+    @Override
+    public boolean isCellEditable(int row, int column) {       
+        return false;
+    }
+    
+    /**
+     * Clears the table
+     */
+    public void clear() {
+    	// update this model's orderLine copies
+    	orderLines.clear();
+    	// Update the rendered table
+    	this.fireTableDataChanged();
+    }
+
+    /**
+     * Removes a row from the table model and the database
+     *
+     * @param row the row
+     */
+    public void remove(int row) {
+    	OrderLine orderLine = orderLines.get(row);
+    	if (orderLine != null) {
+        	// update this model's itemLine copies
+        	orderLines.remove(orderLine);
+        	orderLineCtrl.deleteOrderLine(orderLine);
+        	// Update the rendered table
+        	this.fireTableRowsDeleted(row, row);
+    	}
+    }
+    
+    /**
+     * Adds the orderLine to the table
+     *
+     * @param itemLine the orderLine
+     */
+    public void add(OrderLine orderLine) {
+    	orderLines.add(orderLine);
+    	this.fireTableRowsInserted(this.getRowCount() - 1, this.getRowCount() - 1);
+    }
+    
+}
