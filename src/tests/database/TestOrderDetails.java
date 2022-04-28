a6d14a81d9496800a68d3836fa6d9e7e3585f268
Tomáš Sabol <tomassabol2@gmail.com>
2022-04-28 10:11:58 +0200

    OrderDetails test

---
+++ src/tests/database/TestOrderDetails.java
@@ -0,0 +1,78 @@
+package tests.database;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.*;
+
+import controller.OrderController;
+import controller.OrderDetailsController;
+import controller.OrderLineController;
+import database.DBConnection;
+import exceptions.NotFoundException;
+import model.Order;
+import model.OrderLine;
+
+public class TestOrderDetails {
+    private OrderLineController orderLineCtrl;
+    private OrderController orderCtrl;
+    private OrderDetailsController orderDetailsCtrl;
+
+    @BeforeEach
+    public void setUp() throws SQLException {
+        orderCtrl = new OrderController();
+        orderLineCtrl = new OrderLineController();
+        orderDetailsCtrl = new OrderDetailsController();
+    }
+
+    @Test
+    public void testFindByOrderId1() throws SQLException, NotFoundException {
+        // Arrange
+        int id = 4;
+        List<OrderLine> orderLines;
+        // Act
+        orderLines = orderDetailsCtrl.findByOrderId(id);
+
+        // Assert
+        assertNotNull(orderLines);
+    }
+
+    @Test
+    public void testCreateOrderLineDetails() throws SQLException, NotFoundException {
+        // Arrange
+        OrderLine orderLine = orderLineCtrl.findById(2);
+        Order order = orderCtrl.findById(1);
+        List<OrderLine> orderLines;
+        // Act
+        orderDetailsCtrl.createOrderDetails(order, orderLine);
+        orderLines = orderDetailsCtrl.findByOrderId(order.getId());
+        // Assert
+        assertNotNull(orderLines);
+        assertEquals(2, orderLines.size());
+    }
+
+    @Test
+    public void testDeleteOrderLineDetails() throws SQLException, NotFoundException {
+        // Arrange
+        Order order;
+        List<OrderLine> orderLines;
+        // Act
+        order = orderCtrl.findById(1);
+        orderDetailsCtrl.deleteAllOrderDetails(order);
+        orderLines = orderDetailsCtrl.findByOrderId(order.getId());
+        // Assert
+        assertEquals(0, orderLines.size());
+    }
+    
+
+    @AfterEach
+    public void clearUp() throws SQLException {
+        DBConnection.getInstance().disconnect();
+        orderLineCtrl = null;
+        orderCtrl = null;
+        orderDetailsCtrl = null;
+    }
+    
+}
