b73fa24c307bc6a61c9f85b470ffdf49d5c8b6e3
Boti <budai.boti03@gmail.com>
2022-05-09 17:08:15 +0200

    added code for shelf db/dbif created shelfdb test

--- src/database/ShelfDB.java
+++ src/database/ShelfDB.java
@@ -1,7 +1,108 @@
 package database;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import controller.DepartmentController;
+import controller.ProductController;
 import database.interfaces.ShelfDBIF;
+import exceptions.NotFoundException;
+import model.Department;
+import model.Product;
+import model.Shelf;
+import model.Department;
 
 public class ShelfDB implements ShelfDBIF {
+    //PreparedStatements
+    
+    private static final String FIND_ALL = "select * from Shelves";
+    private static final String FIND_BY_ID = "select * from Shelves where Id = ?";
+    private static final String CREATE_SHELF = "insert into Shelves values(?,?,?,?)";
+    private static final String UPDATE_SHELF = "update Shelves  set Name = ?,DepartmentId = ?";
+    private static final String DISABLE_SHELF = "delete from Shelves where Id = ?";
 
+    private PreparedStatement findAll;
+    private PreparedStatement findById;
+    private PreparedStatement createShelf;
+    private PreparedStatement updateShelf;
+    private PreparedStatement deleteShelf;
+
+    ProductController productController;
+    DepartmentController departmentController;
+
+    public ShelfDB()throws SQLException{
+        findAll = DBConnection.getInstance().getConnection().prepareStatement(FIND_ALL);
+        findById = DBConnection.getInstance().getConnection().prepareStatement(FIND_BY_ID);
+        createShelf = DBConnection.getInstance().getConnection().prepareStatement(CREATE_SHELF, Statement.RETURN_GENERATED_KEYS);
+        updateShelf = DBConnection.getInstance().getConnection().prepareStatement(UPDATE_SHELF);
+        deleteShelf = DBConnection.getInstance().getConnection().prepareStatement(DISABLE_SHELF);
+
+        productController = new ProductController();
+        departmentController  =  new DepartmentController();
+    }
+
+    @Override
+    public List<Shelf> findAll() throws SQLException, NotFoundException{
+        ResultSet rs;
+        rs = findAll.executeQuery();
+        List<Shelf> shelves = buildObjects(rs);
+        return shelves;
+
+    }
+
+    @Override
+    public Shelf findById(int id) throws SQLException, NotFoundException{
+            Shelf shelf = null;
+            ResultSet rs;
+            findById.setInt(1, id);
+            rs = findById.executeQuery();
+            while(rs.next()) {
+                shelf = buildObject(rs);
+            }
+    
+            if (shelf == null) { throw new NotFoundException("Shelf", id); }
+    
+            return shelf;
+    }
+
+    @Override
+    public void createShelf(Shelf shelf) throws SQLException{
+        createShelf.setString(1, shelf.getName());
+        createShelf.setInt(2, shelf.getProduct().getId());
+        createShelf.setInt(3, shelf.getItems().size());
+        createShelf.setInt(4, shelf.getDepartment().getId());
+        shelf.setId(DBConnection.getInstance().executeSqlInsertWithIdentity(createShelf));
+    };
+
+    @Override
+    public void updateShelf(Shelf shelf) throws SQLException{
+        updateShelf.setString(1, shelf.getName());
+        updateShelf.setInt(2, shelf.getProduct().getId());
+        updateShelf.setInt(3, shelf.getItems().size());
+        updateShelf.setInt(4, shelf.getDepartment().getId());
+        updateShelf.executeUpdate();
+    }
+
+    @Override
+    public void deleteShelf(Shelf shelf) throws SQLException{
+        deleteShelf.setInt(1, shelf.getId());
+        deleteShelf.executeUpdate(); 
+    }
+
+    private Shelf buildObject(ResultSet rs)throws SQLException, NotFoundException{
+        Product product = productController.findById(rs.getInt("ProductId"));
+        Department department = departmentController.findById(rs.getInt("DepartmentId"));
+        Shelf shelf = new Shelf(rs.getString("Name"), product, department);
+        shelf.setId(rs.getInt("Id"));
+        return shelf;
+    }
+
+    private List<Shelf> buildObjects(ResultSet rs)throws SQLException, NotFoundException{
+        List<Shelf> shelves = new ArrayList<>();
+        while(rs.next()){
+            shelves.add(buildObject(rs));
+        }
+        return shelves;
+    }
 }
---
+++ src/tests/database/TestShelfDB.java
@@ -0,0 +1,109 @@
+package tests.database;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import java.sql.SQLException;
+import exceptions.NotFoundException;
+import model.Department;
+import model.Product;
+import model.Shelf;
+import database.DBConnection;
+import database.DepartmentDB;
+import database.ProductDB;
+import database.ShelfDB;
+import database.interfaces.DepartmentDBIF;
+import database.interfaces.ProductDBIF;
+import database.interfaces.ShelfDBIF;
+
+public class TestShelfDB {
+    DepartmentDBIF departmentDBIF;
+    ProductDBIF productDBIF;
+    private ShelfDBIF shelfDBIF;
+    private Shelf shelf;
+    Shelf shelf1;  
+    private Shelf newShelf;
+    
+    @BeforeEach
+    public void setup() throws SQLException{
+        shelfDBIF= new ShelfDB();
+        departmentDBIF = new DepartmentDB();
+        productDBIF = new ProductDB();
+    }
+
+    public void findById40() throws SQLException, NotFoundException {
+        shelf1 = shelfDBIF.findById(40);
+    }
+
+    @Test
+    public void testGetShelfById1() throws SQLException, NotFoundException {
+        // Arrange
+        int id = 1;
+        // Act
+        shelf = shelfDBIF.findById(id);
+        // Assert
+        assertNotNull(shelf);
+    }
+
+    @Test
+    public void testGetShelfyById40() throws SQLException, NotFoundException {
+        // Assert
+        assertThrows(NotFoundException.class, () -> findById40());
+    }
+
+    @Test
+    public void testFindAllReturnListSize() throws SQLException,NotFoundException {
+    	// Arrange
+    	int size;
+        // Act
+        size = shelfDBIF.findAll().size();
+
+        // Assert
+        assertNotNull(size);
+    }
+
+    @Test
+    public void testCreateShelf() throws SQLException, NotFoundException {
+        //String name, Product product, Department department
+        // Arrange
+        Department department = departmentDBIF.findById(1);
+        Product product = productDBIF.findById(1);
+        newShelf = new Shelf("TestShelf",product,department );
+        // Act
+        int size = shelfDBIF.findAll().size();
+        shelfDBIF.createShelf(newShelf);
+        // Assert
+        assertEquals(newShelf.getId(), size+1);
+    }
+
+    public void testUpdateCity() throws SQLException, NotFoundException {
+        // Arrange
+        shelf = shelfDBIF.findById(1);
+        // Act
+        shelf.setName("changedName");
+        shelfDBIF.updateShelf(shelf);
+        // Assert
+        assertEquals(shelf.getName(), shelfDBIF.findById(1).getName());
+    }
+
+    @Test
+    public void testDeleteCity() throws SQLException, NotFoundException {
+        // Act
+        int size = shelfDBIF.findAll().size();
+        shelf = shelfDBIF.findById(1);
+        shelfDBIF.deleteShelf(shelf);
+        // Assert
+        assertEquals(size, shelfDBIF.findAll().size()+1);
+    }
+
+    @AfterEach
+    public void clearUp() throws SQLException {
+        DBConnection.getInstance().disconnect();
+        shelf = null;
+        shelf1 = null;
+    }
+}
+
+
+
+
